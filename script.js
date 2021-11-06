f559ad2047e6002186971d870c6716607619771e
Ariadnadpd <ari.ddpd@gmail.com>
2021-11-06 14:07:24 +0000

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // Esto es de prueba, verificar que funciona, sino, eliminarlo.
 $(function(){
     $('.header_dinamico').load("../carga_ficheros/header_footer.html .header1");
@@ -7,7 +9,7 @@ $(function(){
 
 // Funciones para formulario
 
-const email = document.getElementById("mail");
+/*const email = document.getElementById("mail");
 
 email.addEventListener("input", function (event) {
     if (email.validity.typeMismatch) {
@@ -15,6 +17,26 @@ email.addEventListener("input", function (event) {
     } else {
         email.setCustomValidity("");
     }
-});
+});*/
 
+console.log("holaaaaa");
 
+// Funciones para compartir páginas:
+
+// Facebook
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'widgets.js'));
+    
