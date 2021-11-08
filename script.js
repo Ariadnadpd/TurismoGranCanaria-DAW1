d07b6ad83f690ae17f1afb00f89921cd3fd72cbe
Ariadnadpd <ari.ddpd@gmail.com>
2021-11-08 18:34:19 +0000

    first commit

--- script.js
+++ script.js
@@ -40,3 +40,56 @@ console.log("holaaaaa");
         fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'widgets.js'));
     
+
+//Slider manual
+const slider = document.querySelector("#slider");
+let seccionSlider = document.querySelectorAll(".seccion-slider");
+let ultimoSlider = seccionSlider[seccionSlider.length -1];
+
+const botonIzq = document.querySelector("#boton-izquierdo");
+const botonDer = document.querySelector("#boton-derecho");
+
+slider.insertAdjacentElement('afterbegin', ultimoSlider);
+
+function Siguiente() {
+    let primerSlider = document.querySelectorAll(".seccion-slider")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function (){
+        slider.style.transition="none";
+        slider.insertAdjacentElement('beforeend', primerSlider);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+function Anterior() {
+    let seccionSlider = document.querySelectorAll(".seccion-slider");
+    let ultimoSlider = seccionSlider[seccionSlider.length -1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function (){
+        slider.style.transition="none";
+        slider.insertAdjacentElement('afterbegin', ultimoSlider);
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+botonDer.addEventListener('click', function(){
+    Siguiente();
+    pararAuto(); //Paramos el intervalo automático
+    auto = setInterval(Siguiente, 5000); ///Volvemos a iniciar el intervalo automático
+});
+
+botonIzq.addEventListener('click', function(){
+    Anterior();
+    pararAuto(); //Paramos el intervalo automático
+    auto = setInterval(Siguiente, 5000); ///Volvemos a iniciar el intervalo automático
+});
+
+// Slider automático
+var auto = setInterval(Siguiente, 5000);
+
+//Parar ejecución automático cuando se hace click en siguiente
+function pararAuto() {
+    clearInterval(auto);
+}
