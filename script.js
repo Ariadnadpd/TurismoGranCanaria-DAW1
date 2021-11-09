1c6fcf8f1fd3401640aa9fa848ee8f5cf85258ed
Danilooo99 <danilo.informatica.99@gmail.com>
2021-11-09 21:24:04 -0100

    Boton leermas y leermenos

--- script.js
+++ script.js
@@ -80,3 +80,41 @@ function pararAuto() {
 }
 
 // Funciones para formulario
+
+//Botón Leer más
+function leermas1() {
+    $('#boton1').hide();
+    $('.leermas1').css({"display":"block"});
+    $('#botonM1').css({"display":"block"});
+}
+
+function leermas2() {
+    $('#boton2').hide();
+    $('.leermas2').css({"display":"block"});
+    $('#botonM2').css({"display":"block"});
+}
+
+function leermas3() {
+    $('#boton3').hide();
+    $('.leermas3').css({"display":"block"});
+    $('#botonM3').css({"display":"block"});
+}
+
+//Boton leermenos()
+function leermenos1() {
+    $('#botonM1').hide();
+    $('.leermas1').css({"display":"none"});
+    $('#boton1').show();
+}
+
+function leermenos2() {
+    $('#botonM2').hide();
+    $('.leermas2').css({"display":"none"});
+    $('#boton2').show();
+}
+
+function leermenos3() {
+    $('#botonM3').hide();
+    $('.leermas3').css({"display":"none"});
+    $('#boton3').show();
+}
