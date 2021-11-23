6bc2fd0ef0ee3bcd1db91cda29fa16ee5fc62fdb
Ariadnadpd <ari.ddpd@gmail.com>
2021-11-23 15:36:27 +0000

    Mejora de validación de formulario y su estilo

--- scripts/validacion.js
+++ scripts/validacion.js
@@ -1,123 +1,179 @@
 'use strict'
+// Variables globales para manejar los aciertos y errores del formulario
+var acierto = 0;
+var error = 0;
 
-var count = 0;
+$(document).ready(function(){
+    const form = document.getElementById('formulario_contacto');
+    form.addEventListener('submit', e => {
+
+        e.preventDefault();
+
+        // Función que valida la información del formulario
+        validarFormulario();
+        
+        if(acierto == 4) {
+          $("#dialog").dialog("open");      
+        } else if(error == 4){
+          errorDialogo(true);
+        } else if(acierto < 4){
+          acierto=0;
+          errorDialogo(false);
+        }
+        
+    });
+});
+
+// Función para el mensaje de éxito con el objeto dialog() de jQuery-UI
+$(document).ready(function (){
+
+  $("#dialog").dialog({
+    autoOpen: false,
+    modal: true,
+    width: '400px',
+    resizable: false,
+    position:  {  my: " center bottom", at: "center top", of: window },
+    show: {
+      effect: "blind",
+      duration: 1000
+    },
+    hide: {
+      effect: "fade",
+      duration: 1000
+    },
+    buttons: {
+      "Aceptar": function () {
+        location.reload();
+        $(this).dialog("close");
+      }
+    }
+  });
+});
+
+//Función para el mensaje de error con el objeto dialog() de jQuery-UI
+function errorDialogo(variable){
+  var error = "";
+  if (variable==true) {
+    error = "¡No puede enviar el correo vacío! Revise su formulario por favor.";
+  } else {
+    error = "Algunos campos no son correctos o no se han rellenado.. Revise su formulario por favor.";
+  }
+  
+  var dialogo=$('<div></div>');
+  
+  dialogo.text(error);
+  
+  dialogo.dialog({
+    title:'Error al enviar el formulario',
+    width: '400px',
+    modal: true,
+    resizable: false,
+    position:  {  my: "center bottom", at: "center top", of: window },
+    show: {
+      effect: "shake",
+      duration: 1000
+    },
+    hide: {
+      effect: "fade",
+      duration: 1000
+    },
+    buttons: [{
+      text: "Aceptar",
+      click: function() {
+        $(this).dialog("close");
+      }
+    }]
+  });
+};
 
 //Función que valida el formulario de la página de contacto desde la parte del cliente, 
 // antes de enviar la informacíon del mismo al servidor
-function validarFormulario(evento){
-    
-    //Evitamos la acción por defecto del evento
-    if(evento) evento.preventDefault();
+function validarFormulario(evento) {
 
-    //Variables de control y de comprobación
-    
-    var comprueba = false;
-    var control = true;
+   //Evitamos la acción por defecto del evento
+   if(evento) evento.preventDefault();
 
     //Variables generales del formulario
-    let comentario = document.getElementById('comentario1');
-    let errorTxa = document.getElementById('error_textarea'); 
-    let errorSelect = document.getElementById('error_select');
-    let nombreSelect = document.querySelector('select');
-    let nombre = document.getElementById('nombreContacto');
-    let errorNombre = document.getElementById('error_nombre');
-    var regNombre = /^([a-z ñáéíóú]{4,60})$/i;
-    let email = document.getElementById('emailContacto');
-    let errorEmail = document.getElementById('error_email');
-    var regEmail = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    const asunto = document.getElementById('select_contacto');
+    const comentario = document.getElementById('comentario1')
+    const nombre = document.getElementById('nombreContacto');
+    const email = document.getElementById('emailContacto');
+    
+    const nombreValue = nombre.value.trim();
+    const asuntoValue = asunto.value;
+    const comentarioValue = comentario.value.trim();
+    const emailValue = email.value.trim();
 
     // ************************** Select **************************
-    if(nombreSelect.value == "") {
-      errorSelect.innerHTML = "<p>!Debe seleccionar alguna opción!</p>";
-      let error_color_select = document.querySelector("#select_contacto");
-      if(!error_color_select.classList.contains("errorform3")){
-        error_color_select.classList.toggle("errorform3");
-      }
-      control=false;
-      count++;
-      nombreSelect.focus();
-    } else {
-      errorSelect.innerHTML = "";
+    if(asuntoValue === '') {
+        error++;
+        setErrorFor(asunto, '!Debe seleccionar alguna opción!');
+    } else{
+        setSuccessFor(asunto);
     }
 
     // ************************** Textarea **************************
-
-    let error_color_textarea = document.querySelector("#comentario1");
-
-    //TextArea con más de 250 caracteres
-    if(comentario.value.trim().length > 250) {
-        errorTxa.innerHTML='<p>¡El número máximo de caracteres es de 250!</p>';
-        if(!error_color_textarea.classList.contains("errorform2")){
-          error_color_textarea.classList.toggle("errorform2");
-        }
-        control=false;
-        comprueba=true;
-        count++;
-        comentario.focus();
+    //TextArea vacío, con más de 250 caracteres o con menos de 2 caracteres
+    if(comentarioValue === '') {
+        error++;
+        setErrorFor(comentario, '¡El comentario no puede estar en blanco!');
+    } else if(comentarioValue.length > 250){
+        error++;
+        setErrorFor(comentario, '¡El número máximo de caracteres es de 250!');
+    } else if (comentarioValue.length < 2){
+        error++;
+        setErrorFor(comentario, '¡El número mínimo de caracteres es de 2!');
     } else {
-        errorTxa.innerHTML='';
-    }
-    
-    //TextArea vacío o  con menos de 2 caracteres
-    if(comentario.value.trim().length < 2) {
-      errorTxa.innerHTML='<p>¡El número mínimo de caracteres es de 2!</p>';
-      if(!error_color_textarea.classList.contains("errorform2")){
-        error_color_textarea.classList.toggle("errorform2");
-      }
-      control=false;
-      count++;
-      comentario.focus();
-    } else if(!comprueba){
-      errorTxa.innerHTML='';
+        setSuccessFor(comentario);
     }
 
     // ************************** Nombre y apellidos **************************
-
     //Nombre y apellidos que no cumplen con el patrón de un nombre y apellidos
-    if (!regNombre.test(nombre.value)) {
-      errorNombre.innerHTML = '<p>Nombre inválido. Un nombre válido podría ser por ejemplo: Antonio García Rodríguez</p>';
-      let error_color_text = document.querySelector("input[type=text]");
-      if(!error_color_text.classList.contains("errorform")){
-        error_color_text.classList.toggle("errorform");    
-      }
-      control=false;
-      count++;
-      nombre.focus();
+    if(nombreValue === '') { 
+        error++;
+        setErrorFor(nombre, '¡El nombre y apellidos no puede estar en blanco!');	
+    }else if(!isName(nombreValue)) {
+        error++;
+        setErrorFor(nombre, 'Nombre inválido. Un nombre válido podría ser: Antonio García Rodríguez');	
     } else {
-      errorNombre.innerHTML = '';
+        setSuccessFor(nombre);
+    }
+    
+    // ************************** Email **************************
+    //Email correcto siguiendo un patrón
+    if(emailValue === '') {
+        error++;
+        setErrorFor(email, '¡El email no puede estar en blanco!');
+    } else if (!isEmail(emailValue)) {
+        error++;
+        setErrorFor(email, 'Email inválido. Un email válido podría ser: jose.daw.99@gmail.com');
+    } else {
+        setSuccessFor(email);
     }
 
-    // ************************** Email **************************
-    
-    //Email correcto siguiendo un patrón
-    if (!regEmail.test(email.value)) {
-      errorEmail.innerHTML = '<p>Email inválido. Un email válido podría ser por ejemplo: jose.daw.99@gmail.com</p>';
-      let error_color_email = document.querySelector("input[type=email]");
-      if(!error_color_email.classList.contains("errorform1")){
-        error_color_email.classList.toggle("errorform1");
-      }
-      control=false;
-      count++;
-      email.focus();
-    } else {
-      errorEmail.innerHTML = '';
-    }
-    
-    // Valores devueltos por la función
-    if(!control) {
-      if(count == 4){
-        nombreSelect.focus();
-      }
-      count=0;
-      errorDialogo();
-      return false;
-    } else {
-      $("#dialog").dialog("open");      
-    }
-    return true; 
-    
 }
 
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    acierto++;
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';  
+}
+    
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+function isName(nombre){
+    return /^([a-z ñáéíóú]{4,60})$/i.test(nombre);
+}
 
 //Función que cuenta los caracteres restantes a escribir en el cuadro de texto del comentario del formulario.
 function cuenta(){
@@ -129,60 +185,10 @@ function cuenta(){
     negrita.innerHTML = 250 - tam.length;
 
     if(tam.length > 250 || tam.length < 0) {
-      restriccion.style.display="none";
-      errorTxa.innerHTML = "<p>Se ha excedido el número de caracteres permitidos, su formulario no se enviará.</p>";
+        restriccion.style.display="none";
+        errorTxa.innerHTML = "<p>Se ha excedido el número de caracteres permitidos, su formulario no se enviará.</p>";
     } else {
-      restriccion.style.display="block"; 
-      errorTxa.innerHTML="";
+        restriccion.style.display="block"; 
+        errorTxa.innerHTML="";
     }
 }
-
-
-//Función para el mensaje de error con el objeto dialog() de jQuery-UI
-function errorDialogo(){
-  var error = "Algunos campos son incorrectos. Vuelva a revisar su formulario.";
-  var dialogo=$('<div></div>');
-  dialogo.text(error);
-  dialogo.dialog({
-      title:'Error al enviar el formulario',
-      width: '400px',
-      modal: true,
-      resizable: false,
-      buttons: [{
-          text: "Aceptar",
-          click: function() {
-            $(this).dialog("close");
-          }
-      }]
-
-  });
-  
-}
-
-// Función para el mensaje de éxito con el objeto dialog() de jQuery-UI
-$(document).ready(function exitoDialogo(){
-
-//  $("#dialog").text("El correo se ha enviado correctamente.");
-  $("#dialog").dialog({
-    autoOpen: false,
-    autoClose: false,
-    modal: true,
-    width: '400px',
-    resizable: false,
-    buttons: {
-      "Aceptar": function () {
-        location.reload();
-        $(this).dialog("close");
-      }
-    }
-  });
-
-  $("#enviar_form").button().click(function(){
-    validarFormulario();
-    return false;     
-  });
-});
-
-
-
-
